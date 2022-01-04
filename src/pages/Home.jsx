2c807c9df89ee45c18b63901f9e9316c45d3c630
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-01-04 20:11:55 +0000

    UserResults component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from '../components/users/UserResults';
+
+function Home() {
+    return (
+        <>
+            {/* SEARCH COMPONENET GOES HERE */}
+            <UserResults />
+        </>
+    )
+}
+
+export default Home;
